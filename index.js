1e286fd381fd4c8e488795dff3d58480ea8f02c7
Milton Flores <mfloresm10@est.ups.edu.ec>
2025-02-04 12:36:09 -0500

    Añadi Vercel.json

--- index.js
+++ index.js
@@ -7,14 +7,17 @@ app.use(express.json());
 const PORT = 3030;
 app.use(
   cors({
-     origin: "http://localhost:5173",
+      origin: 
+    //   "http://localhost:5173",
+     "examen-frontend-puce.vercel.app",
+
   })
 );
 
 app.use('/api/articulos', articuloRouter);
 
-app.listen(PORT, () => {
-  console.log("Servidor a la espera en el puerto " + PORT);
-}); 
+// app.listen(PORT, () => {
+//   console.log("Servidor a la espera en el puerto " + PORT);
+// }); 
 
 export default app;
